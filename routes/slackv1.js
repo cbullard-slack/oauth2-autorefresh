267961fe789a941ead6c204e87f8e33a2223573a
Callum Bullard <cbullard@slack-corp.com>
2022-05-25 16:24:04 +1000

    Quick deploy minor changes using quickdeploy.sh

--- routes/slackv1.js
+++ routes/slackv1.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const v1 = express.Router();
 
-v1.get('', function(req, res){
-    res.json({ 'success' : true });
+v1.get("", function (req, res) {
+  res.json({ success: true });
 });
 
-v1.get("/auth", (req,res) => {
-    res.send(200);
-    console.log(req.query);
+v1.get("/auth", (req, res) => {
+  if (!req.hasOwnProperty("query")) {
+    res.send(418);
+    return;
+  } else if (!req.query.hasOwnProperty("code"));
+  {
+    res.send(418);
+    return;
+  }
+  res.send(200);
+  console.log(req.query);
 });
 
 module.exports = v1;
