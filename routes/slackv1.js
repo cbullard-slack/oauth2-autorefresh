1220c171c1a9951650b1be461c3051c109662e9f
Callum Bullard <cbullard@slack-corp.com>
2022-05-30 15:55:16 +1000

    Quick deploy minor changes using quickdeploy.sh

--- routes/slackv1.js
+++ routes/slackv1.js
@@ -21,65 +21,6 @@ client.connect();
 
 const API_URL = "https://slack.com/api/";
 
-v1.get("", async (req, res) => {
-  console.log(await PostgresCheckExist(1));
-//   console.log(exists);
-  res.json({ success: false });
-});
-
-v1.get("/auth", (req, res) => {
-  const query = req.query;
-  const params = new URLSearchParams();
-
-  const config = {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: BOT_TOKEN,
-    },
-  };
-
-  params.append("client_id", CLIENT_ID);
-  params.append("client_secret", CLIENT_SECRET);
-  params.append("grant_type", "refresh_token");
-  params.append("refresh_token", REFRESH_TOKEN);
-  console.log(REFRESH_TOKEN);
-  if (!query.hasOwnProperty("code")) {
-    res.status(418).send("No Code");
-    return;
-  }
-  params.append("code", query.code);
-  axios
-    .post(API_URL + "oauth.v2.access", params, config)
-    .then((res) => {
-      const username = res.data.username;
-      const token = res.data.token;
-      const refresh_token = res.data.refresh_token;
-      const time_to_refresh = res.data.time_to_refresh;
-      if (res.data.hasOwnProperty("bot_user_id")) {
-        const id = res.data.bot_user_id;
-        if (PostgresCheckExist(res.data.bot_user_id)) {
-        } else {
-          let date = new Date();
-          Date.prototype.addSecs = (s) => {
-            this.setTime(this.getTime() + s * 1000);
-            return this;
-          };
-          date.addSecs(time_to_refresh);
-          PostgresAddOauth(id, token, refresh_token, date);
-        }
-      } else if (res.data.hasOwnProperty("user_id")) {
-        if (PostgresCheckExist(res.data.user_id)) {
-        }
-      }
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  res.send(200);
-  //   res.redirect('/sharks/shark-facts')
-});
-
 const PostgresCheckExist = async (id) => {
   try {
     console.log(`-=STARTING POSTGRES CHECK EXISTS=-`);
@@ -137,6 +78,65 @@ async function PostgresAddOauth(id, token, refreshToken, time) {
     console.error(err);
   }
 }
+
+v1.get("", async (req, res) => {
+    console.log(await PostgresCheckExist(1));
+  //   console.log(exists);
+    res.json({ success: false });
+  });
+  
+  v1.get("/auth", (req, res) => {
+    const query = req.query;
+    const params = new URLSearchParams();
+  
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: BOT_TOKEN,
+      },
+    };
+  
+    params.append("client_id", CLIENT_ID);
+    params.append("client_secret", CLIENT_SECRET);
+    params.append("grant_type", "refresh_token");
+    params.append("refresh_token", REFRESH_TOKEN);
+    console.log(REFRESH_TOKEN);
+    if (!query.hasOwnProperty("code")) {
+      res.status(418).send("No Code");
+      return;
+    }
+    params.append("code", query.code);
+    axios
+      .post(API_URL + "oauth.v2.access", params, config)
+      .then((res) => {
+        const username = res.data.username;
+        const token = res.data.token;
+        const refresh_token = res.data.refresh_token;
+        const time_to_refresh = res.data.time_to_refresh;
+        if (res.data.hasOwnProperty("bot_user_id")) {
+          const id = res.data.bot_user_id;
+          if (PostgresCheckExist(res.data.bot_user_id)) {
+          } else {
+            let date = new Date();
+            Date.prototype.addSecs = (s) => {
+              this.setTime(this.getTime() + s * 1000);
+              return this;
+            };
+            date.addSecs(time_to_refresh);
+            PostgresAddOauth(id, token, refresh_token, date);
+          }
+        } else if (res.data.hasOwnProperty("user_id")) {
+          if (PostgresCheckExist(res.data.user_id)) {
+          }
+        }
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    res.send(200);
+    //   res.redirect('/sharks/shark-facts')
+  });
 
 module.exports = v1;
 
