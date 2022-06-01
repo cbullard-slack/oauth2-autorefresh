8aad2c30a212eab4b576097a1dce89cae16fd5d3
Callum Bullard <cbullard@slack-corp.com>
2022-06-01 15:21:59 +1000

    Quick deploy minor changes using quickdeploy.sh

--- routes/slackv1.js
+++ routes/slackv1.js
@@ -122,9 +122,7 @@ v1.get("/auth", async (req, res) => {
       Authorization: BOT_TOKEN,
     },
   };
-  const refresh_token = await PostgresGetRefresh(
-    req.query.bot_user_id || req.query.user
-  );
+  const refresh_token = await PostgresGetRefresh(req.query.bot_user_id);
   params.append("client_id", CLIENT_ID);
   params.append("client_secret", CLIENT_SECRET);
   params.append("grant_type", "refresh_token");
